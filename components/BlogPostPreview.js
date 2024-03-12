da2b01140c567fed203298624de1fe9b22dee10d
longevityverse <160720087+longevityverse@users.noreply.github.com>
2024-03-12 20:45:38 +0000

    feat: Add new features to BlogPostPreview component

--- components/BlogPostPreview.js
+++ components/BlogPostPreview.js
@@ -6,7 +6,6 @@ const BlogPostPreview = ({ post }) => (
     <h2>{post.title}</h2>
     <p>{post.summary}</p>
     <Link href={`/blog/${post.slug}`}>Read More</Link>
-    {/* Add these: */}
     <div>
       <button onClick={handleComment}>Comment</button>
       <button onClick={handleShare}>Share</button>
