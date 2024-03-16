c45c17ec13549e22e14459b38eaa85999585eccc
longevityverse <160720087+longevityverse@users.noreply.github.com>
2024-03-16 13:38:13 +0000

    feat: Added useRouter and toast functionality for handling comments and sharing in BlogPostPreview component

--- components/BlogPostPreview.js
+++ components/BlogPostPreview.js
@@ -1,27 +1,37 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { toast } from "react-toastify";
 
-const BlogPostPreview = ({ post }) => (
-  <article>
-    <h2>{post.title}</h2>
-    <p>{post.summary}</p>
-    <Link href={`/blog/${post.slug}`}>Read More</Link>
-    <div>
-      <button onClick={handleComment}>Comment</button>
-      <button onClick={handleShare}>Share</button>
-    </div>
-  </article>
-);
+const BlogPostPreview = ({ post }) => {
+  const router = useRouter();
 
-function handleComment() {
-  router.push(`/blog/${post.slug}#comments`);
-}
+  function handleComment() {
+    router.push(`/blog/${post.slug}#comments`);
+  }
 
-function handleShare() {
-  navigator.clipboard.writeText(window.location.href + `/blog/${post.slug}`).then(() => {
-    toast.success('Link copied to clipboard!');
-  }, (err) => {
-    toast.error('Failed to copy link.');
-  });
-}
+  function handleShare() {
+    navigator.clipboard
+      .writeText(window.location.href + `/blog/${post.slug}`)
+      .then(() => {
+        toast.success("Link copied to clipboard!");
+      })
+      .catch((err) => {
+        console.error("Failed to copy link:", err);
+        toast.error("Failed to copy link.");
+      });
+  }
+
+  return (
+    <article>
+      <h2>{post.title}</h2>
+      <p>{post.summary}</p>
+      <Link href={`/blog/${post.slug}`}>Read More</Link>
+      <div>
+        <button onClick={handleComment}>Comment</button>
+        <button onClick={handleShare}>Share</button>
+      </div>
+    </article>
+  );
+};
 
 export default BlogPostPreview;
